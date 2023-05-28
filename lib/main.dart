649873592397f6844728d2caa3b179765ebae903
GeovanaBZys <geovana.bragafzys@gmail.com>
2023-05-28 19:41:22 -0400

    final commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:great_places/providers/great_places.dart';
+import 'package:great_places/screens/place_detail_screen.dart';
+import 'package:great_places/screens/place_form_screen.dart';
+import 'package:great_places/screens/places_list_screen.dart';
+import 'package:great_places/utils/app_routes.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (ctx) => GreatPlaces(),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Great Places',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSwatch().copyWith(
+            primary: const Color(0xFFFF624F),
+            secondary: const Color(0xFFA787E7),
+            tertiary: const Color(0xFF00B681),
+          ),
+        ),
+        home: const PlacesListScreen(),
+        routes: {
+          AppRoutes.placeForm: (ctx) => const PlaceFormScreen(),
+          AppRoutes.placeDetail: (ctx) => const PlaceDetailScreen(),
+        },
+      ),
+    );
+  }
+}
---
+++ lib/screens/place_form_screen.dart
@@ -0,0 +1,98 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:great_places/providers/great_places.dart';
+import 'package:great_places/widgets/image_input.dart';
+import 'package:great_places/widgets/location_input.dart';
+import 'package:provider/provider.dart';
+
+class PlaceFormScreen extends StatefulWidget {
+  const PlaceFormScreen({super.key});
+
+  @override
+  State<PlaceFormScreen> createState() => _PlaceFormScreenState();
+}
+
+class _PlaceFormScreenState extends State<PlaceFormScreen> {
+  final _titleController = TextEditingController();
+  File? _pickedImage;
+  LatLng? _pickedPosition;
+
+  void _selectImage(File pickedImage) {
+    setState(() {
+      _pickedImage = pickedImage;
+    });
+  }
+
+  void _selectPosition(LatLng pickedPosition) {
+    setState(() {
+      _pickedPosition = pickedPosition;
+    });
+  }
+
+  bool _isValidForm() {
+    return _titleController.text.isNotEmpty &&
+        _pickedImage != null &&
+        _pickedPosition != null; //|| _getCurrentUserLocation != null;
+  }
+
+  void _submitForm() {
+    if (!_isValidForm()) return;
+
+    Provider.of<GreatPlaces>(context, listen: false).addPlace(
+      _titleController.text,
+      _pickedImage!,
+      _pickedPosition!,
+    );
+
+    Navigator.of(context).pop();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('Novo lugar'),
+        ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Column(
+                    children: <Widget>[
+                      TextField(
+                        controller: _titleController,
+                        decoration: const InputDecoration(
+                          labelText: 'Título',
+                        ),
+                        onChanged: (text) {
+                          setState(() {});
+                        },
+                      ),
+                      const SizedBox(height: 10),
+                      ImageInput(_selectImage),
+                      const SizedBox(height: 10),
+                      LocationInput(_selectPosition),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton.icon(
+              icon: const Icon(Icons.add),
+              label: const Text('Adicionar'),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Theme.of(context).colorScheme.tertiary,
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+                elevation: 0,
+              ),
+              onPressed: _isValidForm() ? _submitForm : null,
+            ),
+          ],
+        ));
+  }
+}
